311e9eae59f48b7c06e952801955a470596450b5
Elise <elise.ordoquy@icloud.com>
2026-01-28 18:22:30 +0100

    atténuation

--- src/main.cpp
+++ src/main.cpp
@@ -43,69 +43,72 @@ int main()
     general_light.pos = glm::vec3(-15.0f, 0.0f, -4.0f);
     general_light.color = glm::vec3(0.5f, 0.5f, 0.5f); 
 
-    glm::vec3 candle_light = glm::vec3(0.03f, 0.02f, 0.0f);
+    glm::vec3 candle_light = glm::vec3(0.3f, 0.2f, 0.0f);
 
     //Light candle
     Light yellox;
-    yellox.pos = glm::vec3(1.0f, 0.2f, 0.0f);
+    yellox.pos = glm::vec3(1.0f, -0.2f, 0.0f);
     yellox.color = candle_light;
 
     Light yellox2;
-    yellox2.pos = glm::vec3(sqrt(3)/2, 0.2f, 0.5f);
+    yellox2.pos = glm::vec3(sqrt(3)/2, -0.2f, 0.5f);
     yellox2.color = candle_light;
 
     Light yellox4;
-    yellox4.pos = glm::vec3(0.5f, 0.2f, sqrt(3)/2);
+    yellox4.pos = glm::vec3(0.5f, -0.2f, sqrt(3)/2);
     yellox4.color = candle_light;
 
     Light yellox5;
-    yellox5.pos = glm::vec3(0.0f, 0.2f, 1.0f);
+    yellox5.pos = glm::vec3(0.0f, -0.2f, 1.0f);
     yellox5.color = candle_light;
 
     Light yellox6;
-    yellox6.pos = glm::vec3(-0.5f, 0.2f, sqrt(3)/2);
+    yellox6.pos = glm::vec3(-0.5f, -0.2f, sqrt(3)/2);
     yellox6.color = candle_light;
 
     Light yellox8;
-    yellox8.pos = glm::vec3(-sqrt(3)/2, 0.2f, 0.5f);
+    yellox8.pos = glm::vec3(-sqrt(3)/2, -0.2f, 0.5f);
     yellox8.color = candle_light;
 
     Light yellox9;
-    yellox9.pos = glm::vec3(-1.0f, 0.2f, 0.0f);
+    yellox9.pos = glm::vec3(-1.0f, -0.2f, 0.0f);
     yellox9.color = candle_light;
 
     Light yellox10;
-    yellox10.pos = glm::vec3(-0.5f, 0.2f, -sqrt(3)/2);
+    yellox10.pos = glm::vec3(-0.5f, -0.2f, -sqrt(3)/2);
     yellox10.color = candle_light;
 
     Light yellox12;
-    yellox12.pos = glm::vec3(-sqrt(3)/2, 0.2f, -0.5f);
+    yellox12.pos = glm::vec3(-sqrt(3)/2, -0.2f, -0.5f);
     yellox12.color = candle_light;
 
     Light yellox13;
-    yellox13.pos = glm::vec3(0.0f, 0.2f, -1.0f);
+    yellox13.pos = glm::vec3(0.0f, -0.2f, -1.0f);
     yellox13.color = candle_light;
 
     Light yellox14;
-    yellox14.pos = glm::vec3(sqrt(3)/2, 0.2f, -0.5f);
+    yellox14.pos = glm::vec3(sqrt(3)/2, -0.2f, -0.5f);
     yellox14.color = candle_light;
 
     Light yellox16;
-    yellox16.pos = glm::vec3(0.5f, 0.2f, -sqrt(3)/2);
+    yellox16.pos = glm::vec3(0.5f, -0.2f, -sqrt(3)/2);
     yellox16.color = candle_light;
 
 
     std::vector<Light> light_list = {yellox, yellox2, yellox4, yellox5, yellox6, yellox8, yellox9,
-         yellox10, yellox12, yellox13, yellox14, yellox16};
+        yellox10, yellox12, yellox13, yellox14, yellox16};
+
+    //yellox2, yellox4, yellox5, yellox6, yellox8, yellox9,
+         //yellox10, yellox12, yellox13, yellox14, yellox16
     
 
     //define textures
-    Texture *texture = new Texture("C:/Users/Newteam-Consulting/Documents/infopasgraphique/info_graphique/textures/texture1.png");
+    Texture *texture = new Texture("/Users/eliseordoquy/Programmation/OpenGL/info_graphique/textures/texture1.png");
         // chemin Elise : /Users/eliseordoquy/Programmation/OpenGL/info_graphique/textures/texture1.png
         // chemin Tiph : C:/Users/tipha/Documents/Projet_info_graphique/textures/texture1.png
         // chemin Lina : C:/Users/Newteam-Consulting/Documents/infopasgraphique/info_graphique/textures/texture1.png
 
-    Texture *texture2 = new Texture("C:/Users/Newteam-Consulting/Documents/infopasgraphique/info_graphique/textures/texture2.jpg");
+    Texture *texture2 = new Texture("/Users/eliseordoquy/Programmation/OpenGL/info_graphique/textures/texture2.jpg");
         //chemin Tiph : C:/Users/tipha/Documents/Projet_info_graphique/textures/texture2.jpg
         //chemin Lina : C:/Users/Newteam-Consulting/Documents/infopasgraphique/info_graphique/textures/texture2.jpg
 
@@ -127,13 +130,17 @@ int main()
         texlight_shader->setVec3("light_list[" + std::to_string(i) + "].color", light_list[i].color);
     }
 
+    // Passer la position de la caméra au shader
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.25f, 0.0f);
+    texlight_shader->setVec3("viewPos", cameraPos);
+
+
     Shape* sphere1 = new TexturedSphere(texlight_shader, texture);
     glm::mat4 sphere1_mat = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -4.0f))
         * glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 1.0f))
         * glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
 
-    float candle_height = 0.0001;
     float candle_height = 0.0001;
     float candle_radius = 0.025;
 
@@ -267,7 +274,7 @@ int main()
 
 
     Shape* carre1 = new Carre(texlight_shader,texturecaillou, 50.0, 50.0);
-    glm::mat4 carre1_mat = glm::translate(glm::mat4(1.0f), glm::vec3(-2.0f, -2.0f, 0.0f))
+    glm::mat4 carre1_mat = glm::translate(glm::mat4(1.0f), glm::vec3(-20.0f, -2.0f, 0.0f))
         * glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 1.0f))
         * glm::rotate(glm::mat4(1.0f), glm::radians(45.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
