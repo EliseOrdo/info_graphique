341d87c4cd057d68e1d03121f07de73054198147
Elise <elise.ordoquy@icloud.com>
2026-01-28 23:07:54 +0100

    enlever une erreur

--- src/main.cpp
+++ src/main.cpp
@@ -156,35 +156,34 @@ int main()
     
 
     //define textures
-    Texture *texture = new Texture("C:/Users/tipha/Documents/Projet_info_graphique/textures/texture1.png");
+    Texture *texture = new Texture("/Users/eliseordoquy/Programmation/OpenGL/info_graphique/textures/texture1.png");
         // chemin Elise : /Users/eliseordoquy/Programmation/OpenGL/info_graphique/textures/texture1.png
         // chemin Tiph : C:/Users/tipha/Documents/Projet_info_graphique/textures/texture1.png
         // chemin Lina : C:/Users/Newteam-Consulting/Documents/infopasgraphique/info_graphique/textures/texture1.png
 
-    Texture *texture2 = new Texture("C:/Users/tipha/Documents/Projet_info_graphique/textures/texture2.jpg");
+    Texture *texture2 = new Texture("/Users/eliseordoquy/Programmation/OpenGL/info_graphique/textures/texture2.jpg");
         //chemin Tiph : C:/Users/tipha/Documents/Projet_info_graphique/textures/texture2.jpg
         //chemin Lina : C:/Users/Newteam-Consulting/Documents/infopasgraphique/info_graphique/textures/texture2.jpg
 
-    Texture *texture3 = new Texture("C:/Users/tipha/Documents/Projet_info_graphique/textures/texture3.png");
+    Texture *texture3 = new Texture("/Users/eliseordoquy/Programmation/OpenGL/info_graphique/textures/texture3.png");
 
-    Texture *candle_color = new Texture("C:/Users/tipha/Documents/Projet_info_graphique/textures/candle_color.png");
+    Texture *candle_color = new Texture("/Users/eliseordoquy/Programmation/OpenGL/info_graphique/textures/candle_color.png");
 
-    Texture *texturecaillou = new Texture("C:/Users/tipha/Documents/Projet_info_graphique/textures/murcaillou.png");
+    Texture *texturecaillou = new Texture("/Users/eliseordoquy/Programmation/OpenGL/info_graphique/textures/murcaillou.png");
 
-    Texture *texturevitraille = new Texture("C:/Users/tipha/Documents/Projet_info_graphique/textures/vitrail.png");
+    Texture *texturevitraille = new Texture("/Users/eliseordoquy/Programmation/OpenGL/info_graphique/textures/vitrail.png");
 
     //Texture *candle_color = new Texture("C:/Users/tipha/Documents/Projet_info_graphique/textures/candle_color.png");
 
-    Texture *texturesol = new Texture("C:/Users/tipha/Documents/Projet_info_graphique/textures/texturesol.png");
+    Texture *texturesol = new Texture("/Users/eliseordoquy/Programmation/OpenGL/info_graphique/textures/texturesol.png");
 
 
-    Texture *textureplafond = new Texture("C:/Users/Newteam-Consulting/Documents/infopasgraphique/info_graphique/textures/plafond1.png");
-    Texture *texturebois = new Texture("C:/Users/Newteam-Consulting/Documents/infopasgraphique/info_graphique/textures/boi.png");
-    Texture *texturevitrailmur1 = new Texture("C:/Users/Newteam-Consulting/Documents/infopasgraphique/info_graphique/textures/vitrailmur1.png");
-    Texture *texturevitrailmur2 = new Texture("C:/Users/Newteam-Consulting/Documents/infopasgraphique/info_graphique/textures/vitrailmur2.png");
-    Texture *textureporte = new Texture("C:/Users/Newteam-Consulting/Documents/infopasgraphique/info_graphique/textures/porte.png");
+    Texture *textureplafond = new Texture("/Users/eliseordoquy/Programmation/OpenGL/info_graphique/textures/plafond1.png");
+    Texture *texturebois = new Texture("/Users/eliseordoquy/Programmation/OpenGL/info_graphique/textures/boi.png");
+    Texture *texturevitrailmur1 = new Texture("/Users/eliseordoquy/Programmation/OpenGL/info_graphique/textures/vitrailmur1.png");
+    Texture *texturevitrailmur2 = new Texture("/Users/eliseordoquy/Programmation/OpenGL/info_graphique/textures/vitrailmur2.png");
+    Texture *textureporte = new Texture("/Users/eliseordoquy/Programmation/OpenGL/info_graphique/textures/porte.png");
     Texture *text_blanc = new Texture("/Users/eliseordoquy/Programmation/OpenGL/info_graphique/textures/blanc.png");
-    Texture *text_blanc = new Texture("C:/Users/tipha/Documents/Projet_info_graphique/textures/blanc.png");
 
 
     //add lights in the shader 
